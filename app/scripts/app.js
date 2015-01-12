7152eb30f52b5b22abe3f7f41c0a99c01e9cee96
qLb <qoolbio@gmail.com>
2015-01-12 14:24:51 +0000

    prepared env for frontend ng-app dev

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+' use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiApp
+ * @description
+ * # uiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+// https://github.com/kleingy/angular-simplecouch
